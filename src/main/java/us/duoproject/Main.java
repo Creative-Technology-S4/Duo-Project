55a9632fb55fb00b16d117b94a200dc17a58b5ac
Noah Greff <greffnoah@gmail.com>
2022-10-05 13:05:02 +0200

    added `display` helper method

--- src/main/java/us/duoproject/Main.java
+++ src/main/java/us/duoproject/Main.java
@@ -8,8 +8,13 @@ public class Main {
     private static final int SIZE_Y = 600;
 
     public static void main(String[] args) {
+        MissionControlPanel panel = new MissionControlPanel(SIZE_X, SIZE_Y, 6);
+        display(panel);
+    }
+
+    public static void display(MissionControlPanel panel) {
         JFrame frame = new JFrame();
-        frame.add(new MissionControlPanel(SIZE_X, SIZE_Y, 3));
+        frame.add(panel);
         frame.setSize(SIZE_X, SIZE_Y);
         frame.setVisible(true);
     }
