6d39e22553e9e3b45f4381d1e53327801c292eaa
Noah Greff <greffnoah@gmail.com>
2022-10-05 13:05:32 +0200

    added beacons/tracks beacon implementation with `Marker`

--- src/main/java/us/duoproject/MissionControlPanel.java
+++ src/main/java/us/duoproject/MissionControlPanel.java
@@ -1,12 +1,19 @@
 package us.duoproject;
 
+import us.duoproject.marker.Marker;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MissionControlPanel extends JPanel {
 
     private static final int TRACK_SIZE = 30;
     private static final int BEACON_SIZE = 20;
+
+    private final List<Marker> BEACONS = new ArrayList<>();
+    private final List<Marker> TRACKS = new ArrayList<>();
 
     private final int sizeX;
     private final int sizeY;
@@ -16,6 +23,11 @@ public class MissionControlPanel extends JPanel {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.beaconCount = beaconCount;
+
+        for (int i = 1; i <= beaconCount; i++) {
+            BEACONS.add(new Marker(i, deltaX(i, BEACON_SIZE), deltaY(3), BEACON_SIZE));
+            TRACKS.add(new Marker(i, deltaX(i, TRACK_SIZE), deltaY(4), TRACK_SIZE));
+        }
     }
 
     @Override
@@ -24,16 +36,14 @@ public class MissionControlPanel extends JPanel {
 
         graphics.setColor(new Color(200, 200, 200));
         graphics.fillRect(0, deltaY(3), sizeX, BEACON_SIZE);
-        graphics.setColor(new Color(150, 200, 60));
-        for (int i = 1; i <= beaconCount; i++) {
-            graphics.fillRoundRect(deltaX(i, BEACON_SIZE), deltaY(3), BEACON_SIZE, BEACON_SIZE, BEACON_SIZE, BEACON_SIZE);
+        for (int i = 0; i < beaconCount; i++) {
+            BEACONS.get(i).paint(graphics);
         }
 
         graphics.setColor(new Color(150, 150, 150));
         graphics.fillRect(0, deltaY(4) + TRACK_SIZE / 4, sizeX, TRACK_SIZE / 2);
-        graphics.setColor(new Color(40, 160, 255));
-        for (int i = 1; i <= beaconCount; i++) {
-            graphics.fillRoundRect(deltaX(i, TRACK_SIZE), deltaY(4), TRACK_SIZE, TRACK_SIZE, TRACK_SIZE, TRACK_SIZE);
+        for (int i = 0; i < beaconCount; i++) {
+            TRACKS.get(i).paint(graphics);
         }
     }
 
@@ -48,5 +58,13 @@ public class MissionControlPanel extends JPanel {
 
     private int deltaX(int input, int size) {
         return (sizeX / (beaconCount + 1) * input) - size / 2;
+    }
+
+    public void triggerBeacon(int id, boolean triggered) {
+        BEACONS.stream().filter(beacon -> beacon.getId() == id).findFirst().ifPresent(beacon -> beacon.setTriggered(triggered));
+    }
+
+    public void triggerTrack(int id, boolean triggered) {
+        BEACONS.stream().filter(beacon -> beacon.getId() == id).findFirst().ifPresent(beacon -> beacon.setTriggered(triggered));
     }
 }
