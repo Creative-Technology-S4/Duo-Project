8f5ee1605bbae8c6e1b2fcffb7b06445b2d7c536
Noah Greff <greffnoah@gmail.com>
2022-11-01 17:33:41 +0100

    updated ino code

--- ino/beacon/beacon.ino
+++ ino/beacon/beacon.ino
@@ -5,77 +5,90 @@ const String END_DELIMITER = ">";
  * Counter used to setup sensors dynamically. Sets up INPUT/OUTPUT pins for every sensor and also performs a trigger check.
  *
  * Setup on Arduino:
- * - Sensors can be added starting from pin 2, 3.
+ * - Sensors can be added starting from pin 4, 5.
  * - The first pin is the input (echo) and second the output (trig).
  * - Sensors have ids 1 through n amounts of sensors.
  *
  * Example:
- * - Single sensor:  pins 2 & 3 is sensor with id "1".
- * 
- * - Dual sensor:    pins 2 & 3 is sensor with id "1".
- *                   pins 4 & 5 is sensor with id "2".
+ * - Single sensor:  pins 4 & 5 is sensor with id "1".
  *
- * - Triple sensor:  pins 2 & 3 is sensor with id "1".
- *                   pins 4 & 5 is sensor with id "2".
- *                   pins 6 & 7 is sensor with id "3".
+ * - Dual sensor:    pins 4 & 5 is sensor with id "1".
+ *                   pins 6 & 7 is sensor with id "2".
+ *
+ * - Triple sensor:  pins 4 & 5 is sensor with id "1".
+ *                   pins 6 & 7 is sensor with id "2".
+ *                   pins 8 & 9 is sensor with id "3".
  */
 const int SENSOR_COUNT = 2;
 
-void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    pinMode(3 + i * 2, OUTPUT);
-    pinMode(2 + i * 2, INPUT);
-  }
-}
+void setup()
+{
+    Serial.begin(9600);
 
-void loop() {
-  int sensors[SENSOR_COUNT] = {};
-
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    sensors[i] = isTriggered(i + 1, 3 + i * 2, 2 + i * 2);
-  }
-
-  String data = "[";
-  for (int i = 0; i < sizeof sensors / sizeof sensors[0]; i++) {
-    if (sensors[i]) {
-      data += String(i + 1) + ",";
+    for (int i = 0; i < SENSOR_COUNT; i++)
+    {
+        pinMode(5 + i * 2, OUTPUT);
+        pinMode(4 + i * 2, INPUT);
     }
-  }
-
-  if (data.length() > 1) {  
-    data.remove(data.length() - 1);
-  }
-  data += "]";
-
-  send(toJson("sensors", data));
 }
 
-bool isTriggered(int id, int ping, int echo) {  
-  digitalWrite(ping, LOW);
-  delayMicroseconds(2);
-  digitalWrite(ping, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(ping, LOW);
+void loop()
+{
+    int sensors[SENSOR_COUNT] = {};
 
-  long cm = microsecondsToCentimeters(pulseIn(echo, HIGH));
+    for (int i = 0; i < SENSOR_COUNT; i++)
+    {
+        sensors[i] = isTriggered(i + 1, 5 + i * 2, 4 + i * 2);
+    }
 
-  delay(100);
-  return withinThreshold(cm);
+    String data = "[";
+    for (int i = 0; i < sizeof sensors / sizeof sensors[0]; i++)
+    {
+        if (sensors[i])
+        {
+            data += String(i + 1) + ",";
+        }
+    }
+
+    if (data.length() > 1)
+    {
+        data.remove(data.length() - 1);
+    }
+    data += "]";
+
+    send(toJson("sensors", data));
 }
 
-long microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29 / 2;
+bool isTriggered(int id, int ping, int echo)
+{
+    digitalWrite(ping, LOW);
+    delayMicroseconds(2);
+    digitalWrite(ping, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ping, LOW);
+
+    long cm = microsecondsToCentimeters(pulseIn(echo, HIGH));
+
+    delay(100);
+    return withinThreshold(cm);
 }
 
-bool withinThreshold(long cm) {
-  return cm < 10;
+long microsecondsToCentimeters(long microseconds)
+{
+    return microseconds / 29 / 2;
 }
 
-void send(String raw) {
-  Serial.println(START_DELIMITER + raw + END_DELIMITER);
+bool withinThreshold(long cm)
+{
+    return cm < 10;
 }
 
-String toJson(String key, String value) {
-  return "{\"" + key + "\":" + value + "}";
+void send(String raw)
+{
+    Serial.println(START_DELIMITER + raw + END_DELIMITER);
+}
+
+String toJson(String key, String value)
+{
+    return "{\"" + key + "\":" + value + "}";
 }
