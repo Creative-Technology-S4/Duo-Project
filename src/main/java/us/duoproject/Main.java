0dbd5bedb6c839e177c646cbe06f52ba14373457
Noah Greff <greffnoah@gmail.com>
2022-11-03 20:13:54 +0100

    can now connect to bluetooth car

--- src/main/java/us/duoproject/Main.java
+++ src/main/java/us/duoproject/Main.java
@@ -4,22 +4,26 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private static final String COM_PORT = "/dev/cu.usbmodem2101";
+    private static final String COM_PORT = "COM4";
     private static final int SENSOR_COUNT = 4;
 
     private static final int SIZE_X = 1200;
     private static final int SIZE_Y = 600;
 
     private static final AtomicBoolean DO_STOP = new AtomicBoolean(false);
-
     private static final AtomicBoolean DO_CALIBRATE = new AtomicBoolean(false);
     private static final AtomicInteger TOTAL_LENGTH = new AtomicInteger(0);
 
@@ -28,9 +32,24 @@ public class Main {
         SerialPort port = SerialPort.getCommPort(COM_PORT);
         while (!port.openPort()) { } // continuously attempt to open port
 
-//        try {
-//            String name = "2";
-//            StreamConnection streamConnection = (StreamConnection) Connector.open(name);
+        try {
+            String name = "btspp://202103110461:1;authenticate=true";
+            StreamConnection streamConnection = (StreamConnection) Connector.open(name);
+            OutputStream outputStream = streamConnection.openOutputStream();
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        if (DO_STOP.get()) {
+                            outputStream.write(new byte[] { 'h', 'e', 'l', 'l', 'o' });
+                            DO_STOP.set(false);
+                        }
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            }, 1000);
 
             MissionControlPanel panel = new MissionControlPanel(SIZE_X, SIZE_Y, SENSOR_COUNT);
             JButton button = new JButton(new AbstractAction("Calibrate") {
@@ -43,18 +62,18 @@ public class Main {
             panel.add(button);
 
             SerialPortJsonReader reader = new SerialPortJsonReader.Builder(SerialPort.LISTENING_EVENT_DATA_RECEIVED)
-                    .listener(data -> onJson(null, panel, data))
+                    .listener(data -> onJson(panel, data))
                     .delimiters("<", ">")
                     .build();
             port.addDataListener(reader);
 
             display(panel);
-//        } catch (IOException exception) {
-//            exception.printStackTrace();
-//        }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
-    public static void onJson(StreamConnection streamConnection, MissionControlPanel panel, JsonObject object) {
+    public static void onJson(MissionControlPanel panel, JsonObject object) {
         DO_STOP.set(false);
         int trainToSensor;
 
@@ -65,14 +84,14 @@ public class Main {
         }
 
         float progress = (float) MathUtil.clampedMap(distance, TOTAL_LENGTH.get(), 0, 0, 1.0F);
-        panel.setProgress(progress);
         trainToSensor = (int) MathUtil.clampedMap(progress, 0, 1.0F, 0, SENSOR_COUNT + 1);
+        panel.setProgress(progress);
 
-        JsonArray sensors = object.get("sensors").getAsJsonArray();
-        panel.clearBoard(); // reset all markers
+        panel.clearBoard();
         panel.revalidate();
         panel.repaint();
 
+        JsonArray sensors = object.get("sensors").getAsJsonArray();
         for (int i = 0; i < sensors.getAsJsonArray().size(); i++) {
             int sensor = sensors.getAsJsonArray().get(i).getAsInt();
             panel.triggerBeacon(sensor, true);
@@ -82,11 +101,12 @@ public class Main {
             }
         }
 
-//        System.out.println(DO_STOP.get()); // TODO - logic
+        System.out.println(DO_STOP.get());
     }
 
     public static void display(MissionControlPanel panel) {
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         frame.setSize(SIZE_X, SIZE_Y);
         frame.setVisible(true);
